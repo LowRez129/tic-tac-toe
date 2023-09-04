3723dedfd6c635b4cbd5de88902a4413a591ed71
LowRez129 <114649755+LowRez129@users.noreply.github.com>
2023-09-04 14:31:50 +1200

    Edit js

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -2,7 +2,7 @@ const playerPrototype = (name) => {
     let move_column;
     let move_row;
 
-    const makeMove = (column, row) => {
+    const makeMove = (row, column) => {
         move_column = column;
         move_row = row;
     }
@@ -11,13 +11,13 @@ const playerPrototype = (name) => {
     const getRow = () => move_row; 
     const getValue = () => console.log({move_column, move_row, name});
 
-    return {makeMove, getColumn, getRow, name,getValue};
+    return {makeMove, getColumn, getRow, name, getValue};
 }
 
 const gameBoard = () => {
     const ROW = [new Array(3), new Array(3), new Array(3)];
 
-    const addMove = (column, row, move) => {
+    const addMove = (row, column, move) => {
         ROW[row][column] = move;
     }
         
@@ -27,10 +27,19 @@ const gameBoard = () => {
 
 function Tic_Tac_Toe() {
     const player1 = playerPrototype("X");
+    const player2 = playerPrototype("O");
     const board = gameBoard();
 
-    const BOARD = document.querySelector(".tic-tac-toe > div");
-    console.log(BOARD.getAttribute("class"));
+    const BOARD = document.querySelector(".tic-tac-toe");
+    BOARD.childNodes.forEach((element) => {
+        element.addEventListener("click", () => {
+            if (`${player1.getRow()}-${player1.getColumn()}` == element.getAttribute("class")) {
+                console.log(`${player1.getRow()}-${player1.getColumn()}`, "=", element.getAttribute("class"));
+                element.textContent = player1.name;
+                //player2.makeMove();
+            }
+        }
+    )});
 
     player1.makeMove(0, 2);
     player1.getValue();
