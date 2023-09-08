3fdd9179b870848c425948027972e7e491f58079
LowRez129 <114649755+LowRez129@users.noreply.github.com>
2023-09-09 09:40:09 +1200

    Edit js html

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -15,59 +15,61 @@ const playerPrototype = (name) => {
 }
 
 const gameBoard = () => {
-    const ROW = [new Array(3), new Array(3), new Array(3)];
+    const POSITION = [new Array(3), new Array(3), new Array(3)];
+
+    const getPOSITION = () => POSITION;
 
     const addMove = (row, column, move) => {
-        ROW[row][column] = move;
+        POSITION[row][column] = move;
     }
         
-    const getValue = () => console.log({ROW});
-    return {ROW, addMove, getValue};
-}
-
-function getRandomInt(max) {
-    return (Math.floor(Math.random() * max));
-};
-
-function enemyMove() {
-    const enemy = playerPrototype("O");
-    const board = gameBoard();
-    const BOARD = document.querySelector(".tic-tac-toe");
-
-    do {
-        enemy.makeMove(getRandomInt(3), getRandomInt(3));
-        let board_position = board.ROW[enemy.getRow()][enemy.getColumn()];
-        switch (board_position) {
-            case "O":
-                continue;
-
-            case "X":
-                continue;
-
-            default:
-                let get_div = BOARD.getElementsByClassName(`${enemy.getRow()}-${enemy.getColumn()}`);
-                console.log(get_div);
-                board.addMove(enemy.getRow(), enemy.getColumn(), enemy.name);
-                get_div.textContent = "O";
-                return;
-        }        
-    }
-    while (count != 4);
+    const getValue = () => console.log({POSITION});
+    return {POSITION, getPOSITION, addMove, getValue};
 }
 
 function Tic_Tac_Toe() {
     const player1 = playerPrototype("X");
+    const enemy = playerPrototype("O");
     const board = gameBoard();
-    const BOARD = document.querySelector(".tic-tac-toe");
+    const TIC_TAC_TOE = document.querySelector(".tic-tac-toe");
 
-    let BOARD_children = Array.from(BOARD.children)
+    const getRandomInt = (max) => {
+        return (Math.floor(Math.random() * max));
+    };
+    
+    const enemyMove = () => {
+        do {
+            enemy.makeMove(getRandomInt(3), getRandomInt(3));
+            enemy.getValue();
+            let board_position = board.POSITION[enemy.getRow()][enemy.getColumn()];
+            switch (board_position) {
+                case "O":
+                    console.log("O: taken by enemy");
+                    break;
+    
+                case "X":
+                    console.log("X: taken by player");
+                    break;
+    
+                default:
+                    const div = TIC_TAC_TOE.querySelector(`.r${enemy.getRow()}-c${enemy.getColumn()}`);
+                    div.textContent = enemy.name;
+                    board.addMove(enemy.getRow(), enemy.getColumn(), enemy.name);
+                    //document.querySelector().textContent = "O";
+                    return;
+            }        
+        }
+        while (true);
+    }
+
+    let BOARD_children = Array.from(TIC_TAC_TOE.children)
     BOARD_children.forEach((element) => {
         element.addEventListener("click", () => {
             
             let attribute_row = parseInt(element.getAttribute("data-row"));
             let attribute_column = parseInt(element.getAttribute("data-column"));
 
-            switch (board.ROW[attribute_row][attribute_column]) {
+            switch (board.POSITION[attribute_row][attribute_column]) {
                 case "X":
                     return console.log("X: spot taken");
                 
